6360b1f2bb6840a6ff06559349c4828a3a67a7fe
Izabela <izabela.laszczuk@gmail.com>
2023-05-09 14:36:49 +0200

    Add tailwind to a container

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import CitiesDisplayed from "./components/displayedCities/CitiesDisplayed.jsx";
-import Footer from "./components/Footer.jsx";
-import "./App.css";
+import CitiesDisplayed from "@components/displayedCities/CitiesDisplay";
+import Footer from "@components/Footer";
+import "@/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="everything">
+    <div>
+      <div className="w-[600px] h-[600px] mx-auto my-[100px] rounded-full bg-white shadow-inner sm:rounded-2xl sm:w-96">
         <h1 className="text-center text-4xl pt-16 pb-8 text-rose-500 font-bold">
           World clock
         </h1>
         <CitiesDisplayed />
       </div>
-      <footer>
         <Footer />
-      </footer>
     </div>
   );
 }
